c853c9a19c379def13066cb3e013ce783ee0a56b
Aiden Storey <git@storey.dev>
2023-01-16 22:03:09 -0500

    Break

--- source/game/state/state_assemble.gd
+++ source/game/state/state_assemble.gd
@@ -9,10 +9,12 @@ var __action: int = Action.Select
 var __camera: Camera
 var __camera_origin: Vector3
 var __camera_zoom: Vector3
+var __closest: Part
 var __over: RigidBody
 var __over_offset: Vector3
 var __over_rotation: float
 var __rotating_part: bool
+var __part_origins: Dictionary = {}
 
 
 # Lifecycle methods
@@ -21,6 +23,9 @@ func _init(tree: SceneTree, cube: Cube).(tree, cube) -> void:
 	__camera = _cube.get_viewport().get_camera()
 	__camera_origin = __camera.translation
 	__camera_zoom = (Vector3(0.0, -3.0, 0.0) - __camera_origin).normalized() * 3.0 + __camera_origin
+
+	for part in _cube.parts:
+		__part_origins[part.name] = part.translation
 
 
 func process(delta: float) -> void:
@@ -49,6 +54,9 @@ func _handle_input(delta: float) -> void:
 		__over_rotation = 0.0
 		__action = Action.Select
 		__pan(__camera_origin)
+
+		if __closest:
+			__closest.translation = __part_origins[__closest.name]
 
 	if __action == Action.Place && Input.is_action_just_pressed("rotate_part"):
 		__rotate_part()
@@ -88,6 +96,49 @@ func __place() -> void:
 
 	var axis: Vector3 = (__over.translation - _cube.translation).normalized()
 	__over.rotate(axis, __over_rotation)
+
+	var closest: Part
+	var closest_angle: float = INF
+
+	var dir_over: Vector3 = __over.global_translation - _cube.global_translation
+
+	for part in _cube.parts:
+		var dir_part: Vector3 = part.global_translation - _cube.global_translation
+
+		var angle: float = dir_part.angle_to(dir_over)
+
+		if closest_angle > angle:
+			closest = part
+			closest_angle = angle
+
+	if !closest || closest == __closest:
+		return
+
+	var tween: SceneTreeTween = _cube.create_tween().set_parallel()
+
+	if __closest:
+		__closest.translation = __part_origins[__closest.name]
+#		var old_origin: Vector3 = __part_origins[__closest.name]
+#		tween.tween_property(
+#			__closest,
+#			"translation",
+#			old_origin,
+#			0.1
+#		)
+#
+	__closest = closest
+
+	var origin: Vector3 = __part_origins[__closest.name]
+	var destination: Vector3 = _cube.translation + (origin - _cube.translation) * 1.2
+
+	__closest.translation = destination
+#
+#	tween.tween_property(
+#		__closest,
+#		"translation",
+#		destination,
+#		0.2
+#	)
 
 
 func __rotate_part() -> void:
