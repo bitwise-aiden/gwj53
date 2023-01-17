7c75f8f27cc561c39dd5c581f908491f6d77c3c5
velopman <74576289+velopman@users.noreply.github.com>
2023-01-16 23:33:25 -0500

    Attempt at rotating faces for placement

--- source/game/cube/part.gd
+++ source/game/cube/part.gd
@@ -4,3 +4,14 @@ class_name Part extends KinematicBody
 # Public variables
 
 onready var face_count: int = $mesh.get_child_count()
+
+var face_direction: Vector3
+
+func _ready() -> void:
+	var direction: Vector3 = Vector3.ZERO
+
+	for face in $mesh.get_children():
+		direction += face.translation - translation
+
+	face_direction = direction.normalized()
+	print(face_direction)
