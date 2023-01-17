3ff8c49e3e992b26c4e460e62e40ce0cd909caf5
Aiden Storey <git@storey.dev>
2023-01-16 22:52:01 -0500

    Add initial attach

--- source/game/cube/part.gd
+++ source/game/cube/part.gd
@@ -1,2 +1,6 @@
 class_name Part extends KinematicBody
 
+
+# Public variables
+
+onready var face_count: int = $mesh.get_child_count()
--- source/game/state/state_assemble.gd
+++ source/game/state/state_assemble.gd
@@ -45,24 +45,42 @@ func _handle_input(delta: float) -> void:
 
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		if __action == Action.Select && __over:
-			__over.sleeping = true
 			__action = Action.Place
 			__pan(__camera_zoom)
 	elif __action == Action.Place && !__rotating_part:
-		__over.sleeping = false
+		if __closest:
+			__attach()
+			__closest.translation = __part_origins[__closest.name]
+
+		var direction: Vector3 = (__over.translation - _cube.translation).normalized()
+		__over.linear_velocity = Vector3.ZERO
+		__over.apply_impulse(__over.translation, direction * 1000.0)
+
 		__over_offset = Vector3.ZERO
 		__over_rotation = 0.0
 		__action = Action.Select
 		__pan(__camera_origin)
 
-		if __closest:
-			__closest.translation = __part_origins[__closest.name]
 
 	if __action == Action.Place && Input.is_action_just_pressed("rotate_part"):
 		__rotate_part()
 
 
 # Private methods
+
+func __attach() -> void:
+	var part_collider: CollisionShape = __over.get_child(0)
+	var part_mesh: MeshInstance = __over.get_child(1)
+
+	__over.remove_child(part_collider)
+	__closest.add_child(part_collider)
+
+	__over.remove_child(part_mesh)
+	__closest.add_child(part_mesh)
+
+	part_collider.rotation = Vector3.ZERO
+	part_mesh.rotation = Vector3.ZERO
+
 
 func __intersect(collision_mask: int) -> Dictionary:
 	var mouse_position: Vector2 = _cube.get_viewport().get_mouse_position()
@@ -94,56 +112,60 @@ func __place() -> void:
 	__over.translation = lerp(__over.translation, result["position"] + __over_offset, 0.5)
 	__over.look_at(_cube.translation, Vector3.UP)
 
-	var axis: Vector3 = (__over.translation - _cube.translation).normalized()
-	__over.rotate(axis, __over_rotation)
+	if !__closest:
+		var axis: Vector3 = (__over.translation - _cube.translation).normalized()
+		__over.rotate(axis, __over_rotation)
 
 	var closest: Part
 	var closest_angle: float = INF
 
 	var dir_over: Vector3 = __over.global_translation - _cube.global_translation
+	var face_count: int = __over.get_child(1).get_child_count()
 
 	for part in _cube.parts:
+		if part.get_child_count() > 1:
+			continue
+
+		if part.face_count != face_count:
+			continue
+
 		var dir_part: Vector3 = part.global_translation - _cube.global_translation
 
 		var angle: float = dir_part.angle_to(dir_over)
+
+		if angle > PI * 0.1:
+			continue
 
 		if closest_angle > angle:
 			closest = part
 			closest_angle = angle
 
-	if !closest || closest == __closest:
+	if !closest:
+		if __closest:
+			__closest.translation = __part_origins[__closest.name]
+			__closest = null
+
 		return
 
-	var tween: SceneTreeTween = _cube.create_tween().set_parallel()
+	if closest == __closest:
+		return
 
 	if __closest:
 		__closest.translation = __part_origins[__closest.name]
-#		var old_origin: Vector3 = __part_origins[__closest.name]
-#		tween.tween_property(
-#			__closest,
-#			"translation",
-#			old_origin,
-#			0.1
-#		)
 #
 	__closest = closest
 
 	var origin: Vector3 = __part_origins[__closest.name]
-	var destination: Vector3 = _cube.translation + (origin - _cube.translation) * 1.2
+	var destination: Vector3 = _cube.translation + (origin - _cube.translation) * 1.5
 
 	__closest.translation = destination
-#
-#	tween.tween_property(
-#		__closest,
-#		"translation",
-#		destination,
-#		0.2
-#	)
 
 
 func __rotate_part() -> void:
 	if __rotating_part:
 		return
+
+	__rotating_part = true
 
 	var tween: SceneTreeTween = _cube.create_tween()
 
@@ -158,23 +180,30 @@ func __rotate_part() -> void:
 	tween.tween_interval(0.1)
 
 	var face_count: float = __over.get_child(1).get_child_count()
-	var angle: float = TAU / face_count
 
-	tween.set_ease(Tween.EASE_IN_OUT).tween_method(
-		self,
-		"__rotate_part_rotation",
-		__over_rotation,
-		__over_rotation + angle,
-		0.15
-	)
+	if face_count > 1:
+		var angle: float = TAU / face_count
 
-	tween.set_ease(Tween.EASE_IN).parallel().tween_method(
+		tween.set_parallel()
+		tween.set_ease(Tween.EASE_IN_OUT).tween_method(
+			self,
+			"__rotate_part_rotation",
+			__over_rotation,
+			__over_rotation + angle,
+			0.15
+		)
+
+	tween.set_ease(Tween.EASE_IN).tween_method(
 		self,
 		"__rotate_part_offset",
 		1.0,
 		0.0,
 		0.05
 	)
+
+	yield(tween, "finished")
+
+	__rotating_part = false
 
 
 func __rotate_part_offset(step: float) -> void:
