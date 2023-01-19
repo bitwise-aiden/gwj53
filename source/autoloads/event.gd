7e16d53a5e734b7d073ec1c7b1024fed956c3d7b
Aiden Storey <git@storey.dev>
2023-01-18 19:19:54 -0500

    Add settings manager

--- source/autoloads/event.gd
+++ source/autoloads/event.gd
@@ -9,3 +9,13 @@ signal game_restart()
 
 signal cube_exploded()
 signal cube_scrambled()
+
+signal set_audio_volume()
+signal set_control_cube_left()
+signal set_control_cube_right()
+signal set_control_cube_up()
+signal set_control_cube_down()
+signal set_control_part_left()
+signal set_control_part_right()
+signal set_score_finished()
+signal set_score_unfinished()
